0d9ba2fcd563e66445a0ded4e6982e0012c402ca
Tsundere-Musume <maharjanelvis55@gmail.com>
2025-05-08 13:22:32 -0500

    undotree and harpoon

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -16,6 +16,7 @@ return {
     formatters_by_ft = {
       lua = { "stylua" },
       python = { "ruff" },
+      markdown =  {"prettier"},
     },
     default_format_opts = {
       lsp_format = "fallback",
